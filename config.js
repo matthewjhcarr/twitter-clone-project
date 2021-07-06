f17e14d80666686c1e930b6f00ae20e9030b4ecf
Matthew Carr <matthewjhcarr@gmail.com>
2021-07-06 22:50:19 +0100

    Added unit test for profile routes
    Added unit tests to cover the profile routes
    - Covered GET api/profile/me
    - Covered POST api/profile
    - Covered GET api/profile
    - Covered GET api/profile/user/:user_id
    
    #3

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   testUsers: process.env.TEST_USERS,
   testAuth: process.env.TEST_AUTH,
+  testProfile: process.env.TEST_PROFILE,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiration: process.env.JWT_EXPIRATION
 }
